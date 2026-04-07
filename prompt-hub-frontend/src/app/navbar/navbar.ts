c5ec39d9fc09323658b5b0cafc73a5b04f64ff2b
Conyx Dev <conyxdev@gmail.com>
2026-04-07 16:41:36 +0200

    feat: :sparkles: dark mode set

--- prompt-hub-frontend/src/app/navbar/navbar.ts
+++ prompt-hub-frontend/src/app/navbar/navbar.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core'
-
+import { Component, signal } from '@angular/core'
+import { Button } from 'primeng/button'
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [Button],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
 
-  greet(){
-  console.log('hello world')
+  isDarkMode = signal<boolean>(false)
+
+  toggleDarkMode() {
+    this.isDarkMode.update(value => !value)
+    document.documentElement.classList.toggle('app-dark', this.isDarkMode())
   }
 }
